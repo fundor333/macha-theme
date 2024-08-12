d56cc264f13b4fafaa4ea1566103950212e14129
fundor333 <github@fundor333.com>
2024-08-13 00:32:48 +0200

    Base for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["content/**/*.md", "layouts/**/*.html"],
+	theme: {
+		fontFamily: {
+			sans: ['lexend', 'sans-serif'],
+			serif: ['Dovestype', 'serif'],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
